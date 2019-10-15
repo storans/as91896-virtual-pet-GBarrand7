6bbddccd9cc27bd71ea757fa17cc2edb159054bb
GBarrand@storans.school.nz <GBarrand@storans.school.nz>
2019-10-15 14:46:58 +1300

    Basic program to check if a cat's weight is too little, in a healthy range or too great based on user input - fixed previous errors (not allowing float input) and added formatting when printing

---
+++ add_subtract.py
@@ -0,0 +1,9 @@
+# Program to add or subtract values from the pet's weight
+
+weight = float(input("How much does your pet weigh?"))
+add = float(input("How much weight has your pet gained?"))
+weight += add
+print(weight)
+subtract = float(input("How much weight has your pet lost?"))
+weight -= subtract
+print(weight)
