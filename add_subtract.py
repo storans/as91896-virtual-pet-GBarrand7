6a7f03be11506a33c372d6e96bb6bd38618455ff
GBarrand@storans.school.nz <GBarrand@storans.school.nz>
2019-10-16 14:17:20 +1300

    Trialling using separate functions to add and subtract rather than doing both in one

--- add_subtract.py
+++ add_subtract.py
@@ -1,25 +1,27 @@
 # Program to add or subtract values from the pet's weight
 # Function to increase or decrease the cat's weight
 
-# Parameters - sign indicates whether the program should add or subtract, amount specifies the amount of weight to be added or subtracted, weight specifies the weight of the pet
-def add_subtract(sign, amount, weight):
-        if sign == "add":
-            weight += amount
-            return weight
-        elif sign == "subtract":
-            weight -= amount
-            return weight
+# Parameters - amount specifies the amount of weight to be added or subtracted, weight specifies the weight of the pet
+# Function to increase the pet's weight
+def add(amount, weight):
+    weight += amount
+    return weight
+
+# Function to decrease the pet's weight
+def subtract(amount, weight):
+    weight -= amount
+    return weight
 
 # Testing of function
 # Getting user input for the amount that their cat weighs
 weight = float(input("How much does your pet weigh?"))
 # Getting user input for the amount of weight to add
-add = float(input("How much weight has your pet gained?"))
+increase = float(input("How much weight has your pet gained?"))
 # Increasing the cat's weight by the specified amount
-weight = add_subtract("add", add, weight)
+weight = add(increase, weight)
 print(weight)
 # Getting user input for the amount of weight to subtract
-subtract = float(input("How much weight has your pet lost?"))
+decrease = float(input("How much weight has your pet lost?"))
 # Decreasing the cat's weight by a specified amount
-weight = add_subtract("subtract", subtract, weight)
+weight = subtract(decrease, weight)
 print(weight)
