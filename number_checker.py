afc04d38c23cd14b74a19aae276a7c9a2e498c0a
GBarrand@storans.school.nz <GBarrand@storans.school.nz>
2019-10-17 11:48:55 +1300

    Created a function to check whether a number is between two values and that a number is entered

--- number_checker.py
+++ number_checker.py
@@ -1,9 +1,17 @@
 # Program to check if a pet's weight is within a certain range
 
-# High and low are constants as they are not changed in this particular program
-
-HIGH = 4.5
-LOW = 3.5
+# Function to check if a number is between two values
+def number_checker(high, low, error, question):
+    valid = False
+    while valid == False:
+        try:
+            response = float(input("{} >> ".format(question)))
+            if low <= response <= high:
+                return response
+            else:
+                print(error)
+        except ValueError:
+            print(error)
 
 # Keep going variable used to create loop for testing
 keep_going = ""
@@ -11,19 +19,12 @@ keep_going = ""
 # Loop to allow for continual testing of values
 while keep_going == "":
     # Getting input from the user for their cat's weight
-    weight = float(input("How much does your cat weigh?"))
+    weight = number_checker(4.5, 3.5, "Please enter a weight between 3.5kg - 4.5kg", "How much does your pet weigh?")
+    print("You pet weighs {}kg".format(weight))
     print("")
-    # Weight is greater than or equal to 3.5 or less than or equal to 4.5 as these are still healthy weights
-    if LOW <= weight <= HIGH:
-        print("Your cat is in a healthy weight range")
-        print("")
-    elif weight < LOW:
-        print("Your cat is underweight")
-        print("")
-    elif weight > HIGH:
-        print("Your cat is overweight")
-        print("")
 
     # Program asks for input in order to either continue or break the loop
     keep_going = input("If you would like to keep going press <enter>")
     print("")
+
+
