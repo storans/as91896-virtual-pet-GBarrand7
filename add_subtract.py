204c794212b200082967f30cccae71bdbd402c39
GBarrand@storans.school.nz <GBarrand@storans.school.nz>
2019-10-16 13:50:16 +1300

    CORRECT TITLE - Basic program to increase or decrease the cat's weight by a user specified amount - added additional comments

--- add_subtract.py
+++ add_subtract.py
@@ -1,10 +1,12 @@
 # Program to add or subtract values from the pet's weight
 
+# Getting user input for the amount of weight to add
 weight = float(input("How much does your pet weigh?"))
 add = float(input("How much weight has your pet gained?"))
 # Increasing the cat's weight by the specified amount
 weight += add
 print(weight)
+# Getting user input for the amount of weight to subtract
 subtract = float(input("How much weight has your pet lost?"))
 # Decreasing the cat's weight by a specified amount
 weight -= subtract
