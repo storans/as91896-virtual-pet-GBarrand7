d7efed25036a9f5ed25bd63cb82238d6d3c440b3
GBarrand@storans.school.nz <GBarrand@storans.school.nz>
2019-10-18 11:42:47 +1300

    Program allowing the user to select the gender of the name they would like and have a random name generated from a 2D list

--- random_name_generator.py
+++ random_name_generator.py
@@ -7,14 +7,21 @@ import random
 # Trialling using a 2D list to store the names
 # First list holds girls' names, second list holds boys' names and third list hold names which could be either gender
 names = [["Pixie", "Babushka", "Poppy", "Molly", "Tina", "Tinkerbell"], ["Lord Meowsworth", "Chad", "Ernie", "Clyde", "Mr Cat", "Garfield"], ["Mittens", "Fluffy", "Hairball", "Fleabag", "Minty", "Whiskers", "Ginger"]]
-# List of numbers which will be randomly selected from in order to select a list from names
-numbers = [0, 1, 2]
 
-# Loop for testing
-for i in range(5):
-    # Randomly generating a number which corresponds to a list in names
-    number = random.choice(numbers)
-    print(number)
-    # Randomly generating a name from the selected list
-    name = random.choice(names[number])
-    print(name)
+keep_going = ""
+
+while keep_going == "":
+    gender = input("Would you like a girl's name or a boy's name? Press <g> for girl, <b> for boy or <n> for a gender neutral name >>")
+    if gender == "g":
+        # Randomly generating a name from the girl's name list
+        name = random.choice(names[0])
+    elif gender == "b":
+        # Randomly generating a name from the boy's name list
+        name = random.choice(names[1])
+    elif gender == "n":
+        # Randomly generating a name from the neutral names list
+        name = random.choice(names[2])
+
+    print("Your cat's name is {}".format(name))
+
+    keep_going = input("Would you like to keep going? Press <enter> to continue")
