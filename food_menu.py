c2627aad27bc13763d425905fe367e252d7ec8bf
GBarrand@storans.school.nz <GBarrand@storans.school.nz>
2019-10-21 09:38:16 +1300

    Formatting printing the foods and their weight gain values in a numbered list - allow user to choose what food they would like their cat to eat

--- food_menu.py
+++ food_menu.py
@@ -6,3 +6,13 @@ for food, amount in FOODS.items():
     print("{}. {} - {}".format(number, food, amount))
     number += 1
 
+# Loop for testing purposes
+for i in range(3):
+    food_choice = int(input("Please enter the corresponding number to the food you would like your pet to eat >> "))
+    if food_choice == 1:
+        print("You cat has eaten salmon and has gained {}kg".format(FOODS["salmon"]))
+    elif food_choice == 2:
+        print("You cat has eaten jelly meat and has gained {}kg".format(FOODS["jelly meat"]))
+    elif food_choice == 3:
+        print("You cat has eaten biscuits and has gained {}kg".format(FOODS["biscuits"]))
+
