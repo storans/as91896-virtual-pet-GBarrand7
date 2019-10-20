4b2722b2e7c3c29d88f17b1039c91f7a48255f90
GBarrand@storans.school.nz <GBarrand@storans.school.nz>
2019-10-20 21:10:53 +1300

    Program to randomly select a name from a specified dictionary of names of a certain gender based on user input - realised there is no point in further developing the program using the dictionary as it provides no benefits over simply putting each gender of name in a separate listing and randomly selecting a name from that list. Using a dictionary simply over-complicates the program unnecessarily

--- random_name_generator_dictionary.py
+++ random_name_generator_dictionary.py
@@ -4,13 +4,28 @@ import random
 
 # Trialling using a dictionary to store the names
 # names = {"Pixie":"g", "Babushka":"g", "Poppy":"g", "Molly":"g", "Tina":"g", "Tinkerbell":"g", "Lord Meowsworth":"b", "Chad": "b", "Ernie": "b", "Clyde": "b", "Mr Cat": "b", "Garfield": "b", "Mittens": "n", "Fluffy": "n", "Hairball": "n", "Fleabag": "n", "Minty": "n", "Whiskers": "n", "Ginger": "n"}
-names = {"g": "Pixie", "g": "Babushka", "b": "Clyde", "b": "Garfield", "n": "Whiskers", "n": "Minty"}
 
-# List of available genders
-genders = ["g", "b", "n"]
+# Dictionary of girls names
+girls = {1: "Pixie", 2: "Babushka"}
+# Dictionary of boys names
+boys = {1: "Clyde", 2: "Garfield"}
+# Dictionary of gender neutral names
+neutral = {1: "Whiskers", 2: "Minty"}
+# List of numbers corresponding to names - only need one list at present as there is the same number of each type of name
+numbers = [1, 2]
 
 # Loop for testing
-for i in range(5):
-    gender = random.choice(genders)
-    name = random.choice(names[gender])
+for i in range(3):
+    gender = input("What type of name would you like? For a girl's name type <g>, for a boy's name type <b> or for a gender neutral name type <n>")
+    number = random.choice(numbers)
+    if gender == "g":
+        name = girls[number]
+        print(name)
+    elif gender == "b":
+        name = boys[number]
+        print(name)
+    else:
+        name = neutral[number]
+        print(name)
+
 
