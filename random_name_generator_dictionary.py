81068b49471f3ca5813f5745e4eaa5ac945a5b87
GBarrand@storans.school.nz <GBarrand@storans.school.nz>
2019-10-20 20:39:13 +1300

    Program to allow user to randomly generate a name using a dictionary - tested printing all of the names from the dictionary

---
+++ random_name_generator_dictionary.py
@@ -0,0 +1,5 @@
+# Trialling using a dictionary to store the names
+names = {"Pixie":"g", "Babushka":"g", "Poppy":"g", "Molly":"g", "Tina":"g", "Tinkerbell":"g", "Lord Meowsworth":"b", "Chad": "b", "Ernie": "b", "Clyde": "b", "Mr Cat": "b", "Garfield": "b", "Mittens": "n", "Fluffy": "n", "Hairball": "n", "Fleabag": "n", "Minty": "n", "Whiskers": "n", "Ginger": "n"}
+
+for name in names:
+    print(name)
