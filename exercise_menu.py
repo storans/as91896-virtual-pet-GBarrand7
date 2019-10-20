84ffefc571f042d0cc1ff97cb929db12679c0d00
GBarrand@storans.school.nz <GBarrand@storans.school.nz>
2019-10-21 09:29:15 +1300

    Formatting printing the exercises and their weight loss values in a numbered list

--- exercise_menu.py
+++ exercise_menu.py
@@ -1,4 +1,8 @@
+# Dictionary of possible exercises
 EXERCISE={"chase a mouse": 0.4, "climb a tree": 0.3, "scratch a post": 0.2}
-
-for activity in EXERCISE:
-    print(activity)
+# Starting value for numbered list
+number = 1
+print("Here are the exercises your cat can do and how much weight it will lose if it does them: ")
+for activity, amount in EXERCISE.items():
+    print("{}. {} - {}".format(number, activity, amount))
+    number += 1
