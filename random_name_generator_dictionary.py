58fc305b08f7e3a5a6ef81fda04d711efeb393cc
GBarrand@storans.school.nz <GBarrand@storans.school.nz>
2019-10-20 20:59:33 +1300

    Failed testing of randomly selecting a name from the dictionary base on gender - realised I would have to split it into 3 separate dictionaries - 1 for girls' name, 1 for boys' names and 1 for gender neutral names

--- random_name_generator_dictionary.py
+++ random_name_generator_dictionary.py
@@ -1,6 +1,16 @@
-# Trialling using a dictionary to store the names
-names = {"Pixie":"g", "Babushka":"g", "Poppy":"g", "Molly":"g", "Tina":"g", "Tinkerbell":"g", "Lord Meowsworth":"b", "Chad": "b", "Ernie": "b", "Clyde": "b", "Mr Cat": "b", "Garfield": "b", "Mittens": "n", "Fluffy": "n", "Hairball": "n", "Fleabag": "n", "Minty": "n", "Whiskers": "n", "Ginger": "n"}
+#Importing random module
 
-# Testing printing all the names within the dictionary
-for name in names:
-    print(name)
+import random
+
+# Trialling using a dictionary to store the names
+# names = {"Pixie":"g", "Babushka":"g", "Poppy":"g", "Molly":"g", "Tina":"g", "Tinkerbell":"g", "Lord Meowsworth":"b", "Chad": "b", "Ernie": "b", "Clyde": "b", "Mr Cat": "b", "Garfield": "b", "Mittens": "n", "Fluffy": "n", "Hairball": "n", "Fleabag": "n", "Minty": "n", "Whiskers": "n", "Ginger": "n"}
+names = {"g": "Pixie", "g": "Babushka", "b": "Clyde", "b": "Garfield", "n": "Whiskers", "n": "Minty"}
+
+# List of available genders
+genders = ["g", "b", "n"]
+
+# Loop for testing
+for i in range(5):
+    gender = random.choice(genders)
+    name = random.choice(names[gender])
+
