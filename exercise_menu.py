1a28c03e86a441e1ed110e8b7b6934d44b15b3e8
GBarrand@storans.school.nz <GBarrand@storans.school.nz>
2019-10-21 14:50:41 +1300

    Allowing the user to select an activity that they would like their cat to do and informing them as to what their pet has done and how much weight they have lost

--- exercise_menu.py
+++ exercise_menu.py
@@ -6,3 +6,13 @@ print("Here are the exercises your cat can do and how much weight it will lose i
 for activity, amount in EXERCISE.items():
     print("{}. {} - {}".format(number, activity, amount))
     number += 1
+
+# Loop for testing purposes
+for i in range(3):
+    exercise_choice = int(input("Please enter the corresponding number to the exercise you would like your pet to do >> "))
+    if exercise_choice == 1:
+        print("You cat has chased a mouse and has lost {}kg".format(EXERCISE["chase a mouse"]))
+    elif exercise_choice == 2:
+        print("You cat has climbed a tree and has gained {}kg".format(EXERCISE["climb a tree"]))
+    elif exercise_choice == 3:
+        print("You cat has scratched a post and has gained {}kg".format(EXERCISE["scratch a post"]))
