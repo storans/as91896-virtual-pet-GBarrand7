938b31bf522c1e7615e01d15340ea71eb8c30c06
GBarrand@storans.school.nz <GBarrand@storans.school.nz>
2019-10-22 21:32:53 +1300

    Basic home menu content and layout

---
+++ home_menu.py
@@ -0,0 +1,13 @@
+# Home menu and start of the game
+
+# Game welcome message
+print("Hello, and welcome to the virtual cat simulator!\n"
+      "In this game you can name your cat, feed it and exercise it.\nBut, be careful!\nIf your cat gets too skinny or too fat it can die, so make sure your cat stays in a healthy weight range of 3.5-4.5kg.\nWe hope you enjoy the game!")
+
+# The users options of activities to do
+print("1. Exercise you cat")
+print("2. Feed your cat")
+print("3. Check your cat's weight")
+print("4. See the game instructions")
+print("5. Quit")
+
