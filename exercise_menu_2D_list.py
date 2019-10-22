068a9547b6d27b02c2fa9728da09fd54eea5edad
GBarrand@storans.school.nz <GBarrand@storans.school.nz>
2019-10-22 14:35:05 +1300

    Printing each list in a 2D list of exercises and their weight loss values

---
+++ exercise_menu_2D_list.py
@@ -0,0 +1,6 @@
+# 2D list of exercises and their weight loss value
+exercises = [["chase a mouse", 0.4], ["climb a tree", 0.3], ["scratch a post", 0.2]]
+
+# Printing each list in the 2D list
+for exercise in exercises:
+    print(exercise)
