82b4df36194ea1fe287a7110c67980fc21857587
GBarrand@storans.school.nz <GBarrand@storans.school.nz>
2019-10-22 14:45:27 +1300

    Formatted printing each list in a 2D list of foods and their weight gain values in a numbered list

--- food_menu_2D_list.py
+++ food_menu_2D_list.py
@@ -1,6 +1,11 @@
+# Setting the starting number for the numbered list
+number = 1
 # 2D list of foods and their weight gain value
 foods = [["salmon", 0.4], ["jelly meat", 0.3], ["biscuits", 0.2]]
 
-# Printing each list in the 2D food list
+print("Here are the foods available and how much weight your cat will gain if it eats them:")
+# Printing each food and it's corresponding weight gain value from the 2D list in a numbered list
 for food in foods:
-    print(food)
+    print("{}. {} - {}".format(number, food[0], food[1]))
+    # Increasing the number for the numbered list
+    number += 1
