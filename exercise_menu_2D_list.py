650cbab2d5d5f8fefe6f84fda176323c6336fc13
GBarrand@storans.school.nz <GBarrand@storans.school.nz>
2019-10-22 14:46:06 +1300

    Formatted printing each list in a 2D list of exercises and their weight loss values in a numbered list

--- exercise_menu_2D_list.py
+++ exercise_menu_2D_list.py
@@ -1,6 +1,12 @@
 # 2D list of exercises and their weight loss value
 exercises = [["chase a mouse", 0.4], ["climb a tree", 0.3], ["scratch a post", 0.2]]
 
-# Printing each list in the 2D list
+# Setting the starting number for the numbered list
+number = 1
+
+print("Here are the exercises available and how much weight your cat will lose if it does them:")
+# Printing each food and it's corresponding weight gain value from the 2D list in a numbered list
 for exercise in exercises:
-    print(exercise)
+    print("{}. {} - {}".format(number, exercise[0], exercise[1]))
+    # Increasing the number for the numbered list
+    number += 1
