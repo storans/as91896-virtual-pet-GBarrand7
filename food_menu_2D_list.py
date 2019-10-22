43a8fe9ecfee20f0fef909bfaddc6cd90613096c
GBarrand@storans.school.nz <GBarrand@storans.school.nz>
2019-10-22 14:35:30 +1300

    Printing each list in a 2D list of foods and their weight gain values

---
+++ food_menu_2D_list.py
@@ -0,0 +1,6 @@
+# 2D list of foods and their weight gain value
+foods = [["salmon", 0.4], ["jelly meat", 0.3], ["biscuits", 0.2]]
+
+# Printing each list in the 2D food list
+for food in foods:
+    print(food)
