3e411a00506efc705e93be2eb423ace8fe0a87dc
GBarrand@storans.school.nz <GBarrand@storans.school.nz>
2019-10-23 13:20:05 +1300

    Fixed typo

--- home_menu.py
+++ home_menu.py
@@ -5,7 +5,7 @@ print("Hello, and welcome to the virtual cat simulator!\n"
       "In this game you can name your cat, feed it and exercise it.\nBut, be careful!\nIf your cat gets too skinny or too fat it can die, so make sure your cat stays in a healthy weight range of 3.5-4.5kg.\nWe hope you enjoy the game!")
 
 # The users options of activities to do
-print("1. Exercise you cat")
+print("1. Exercise your cat")
 print("2. Feed your cat")
 print("3. Check your cat's weight")
 print("4. See the game instructions")
