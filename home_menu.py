db21409f620100cf2a7561cab5278778e77ededd
GBarrand@storans.school.nz <GBarrand@storans.school.nz>
2019-10-23 13:07:31 +1300

    Basic home menu allowing user to choose what they would like to do from the menu

--- home_menu.py
+++ home_menu.py
@@ -11,3 +11,28 @@ print("3. Check your cat's weight")
 print("4. See the game instructions")
 print("5. Quit")
 
+keep_going = ""
+valid = False
+# Loop for testing purposes
+while keep_going == "":
+      while valid == False:
+            activity_choice = input("What would you like to do? Select the corresponding number from the menu above >>")
+            if activity_choice == "1":
+                  print("Let's exercise your cat!")
+                  valid = True
+            elif activity_choice == "2":
+                  print("Let's feed your cat!")
+                  valid = True
+            elif activity_choice == "3":
+                  print("Let's check your cat's weight!")
+                  valid = True
+            elif activity_choice == "4":
+                  print("Here are the instructions...")
+                  valid = True
+            elif activity_choice == "5":
+                  print("Thanks for playing!")
+                  valid = True
+            else:
+                  print("Please enter either 1, 2, 3, 4 or 5")
+      keep_going = input("Press <enter> to continue")
+      valid = False
