24d9e11bbd71a3891def8a439292a3cb278c9405
GBarrand@storans.school.nz <GBarrand@storans.school.nz>
2019-10-30 12:02:04 +1300

    Added error checking for if the user enters a number outside of the desired range of 1-3 - will check for value errors using integer checking function in assembled outcome

--- food_menu.py
+++ food_menu.py
@@ -6,13 +6,19 @@ for food, amount in FOODS.items():
     print("{}. {} - {}".format(number, food, amount))
     number += 1
 
-# Loop for testing purposes
-for i in range(3):
+valid = False
+# Loop that runs until user enters a valid input
+while valid == False:
     food_choice = int(input("Please enter the corresponding number to the food you would like your pet to eat >> "))
     if food_choice == 1:
         print("You cat has eaten salmon and has gained {}kg".format(FOODS["salmon"]))
+        valid = True
     elif food_choice == 2:
         print("You cat has eaten jelly meat and has gained {}kg".format(FOODS["jelly meat"]))
+        valid = True
     elif food_choice == 3:
         print("You cat has eaten biscuits and has gained {}kg".format(FOODS["biscuits"]))
+        valid = True
+    else:
+        print("Please enter either 1, 2, or 3")
 
