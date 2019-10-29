9da0df9e589dff7a48aeee87609c8f19ae136a22
GBarrand@storans.school.nz <GBarrand@storans.school.nz>
2019-10-30 10:16:11 +1300

    Added error checking for the yes/no inputs - if the user doesn't enter any of the expected options

--- input_converter_function.py
+++ input_converter_function.py
@@ -2,29 +2,54 @@
 
 import random
 
+# Function to randomly generate a name
+# Parameters - list_segment - what part of the 2D list the name is generated from based on the desired gender, list_name - the list the names are stored in, message - asking the user if they like the name or not
 def name_generator(list_segment, list_name, message):
+    # Setting the generate variable
     generate = ""
+    # Names are generated while generate = ""
     while generate == "":
+        # Randomly generating a name for the chosen gender
         name = random.choice(list_name[list_segment])
         print(name)
-        generate = input("{}".format(message))
-        if generate in ["yes", "yeah", "y", "sure"]:
-            generate = "yes"
-        elif generate in ["no", "n", "nah", ""]:
-            generate = ""
+        valid = False
+        while valid == False:
+            # Asking if the user likes the name generated
+            generate = input("{}".format(message))#.strip().lower()
+            # Converting the affirmative inputs into the desired input
+            if generate in ["yes", "yeah", "y", "sure"]:
+                generate = "yes"
+                valid = True
+            # Converting the negative inputs into the desired input
+            elif generate in ["no", "n", "nah", ""]:
+                generate = ""
+                valid = True
+            else:
+                print("Please enter either yes or no")
+
     return name
 
+# Function to convert user's gender input to the desired input if their input is within an approved list
+# Parameters - question - the question asked, error - the error message if the user enters none of the desired inputs
 def input_converter(question, error):
+    # Setting valid to False
     valid = False
+    # Function asks for input until the user inputs a valid response
     while valid == False:
-        gender = input(question)
+        gender = input(question).strip().lower()
+        # Checking if the gender is within the approved list of inputs for female gender
         if gender in genders[0]:
+            # Changing input to the standard input
             gender = "g"
             return gender
+        # Checking if the gender is within the approved list of inputs for male gender
         elif gender in genders[1]:
+            # Changing input to the standard input
             gender = "b"
             return gender
+        # Checking if the gender is within the approved list of inputs for gender neutral
         elif gender in genders[2]:
+            # Changing input to the standard input
             gender = "n"
             return gender
         else:
