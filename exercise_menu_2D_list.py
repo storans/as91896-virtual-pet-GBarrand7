c45616c11ab6b9d2462e22611e589b4a26579749
GBarrand@storans.school.nz <GBarrand@storans.school.nz>
2019-10-31 09:29:21 +1300

    Added comments to describe the program more accurately

--- exercise_menu_2D_list.py
+++ exercise_menu_2D_list.py
@@ -1,3 +1,4 @@
+# Program to print the exercise menu and allow the user to choose an exercise from it for their pet to do
 # 2D list of exercises and their weight loss value
 exercises = [["chase a mouse", 0.4], ["climb a tree", 0.3], ["scratch a post", 0.2]]
 
@@ -10,7 +11,7 @@ for exercise in exercises:
     print("{}. {} - {}".format(number, exercise[0], exercise[1]))
     # Increasing the number for the numbered list
     number += 1
-# Setting valid to false
+# Setting valid to false to run the loop
 valid = False
 # Asking for input until user enters a valid input
 while valid == False:
@@ -19,12 +20,16 @@ while valid == False:
     # Providing feedback based on the exercise chosen by the user
     if exercise_choice == 1:
         print("You cat has chased a mouse and has lost {}kg".format(exercises[0][1]))
+        # Breaking the loop as user has entered a valid input
         valid = True
     elif exercise_choice == 2:
         print("You cat has climbed a tree and has lost {}kg".format(exercises[1][1]))
+        # Breaking the loop as user has entered a valid input
         valid = True
     elif exercise_choice == 3:
         print("You cat has scratched a post and has lost {}kg".format(exercises[2][1]))
+        # Breaking the loop as user has entered a valid input
         valid = True
+    # Branch which runs if the user enters an integer outside of the desired range
     else:
         print("Please enter either 1, 2 or 3")
