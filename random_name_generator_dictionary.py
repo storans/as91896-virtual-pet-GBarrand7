c3a72af27f68e1851e64ce07f920b41ba54ae7a9
GBarrand@storans.school.nz <GBarrand@storans.school.nz>
2019-10-31 12:35:41 +1300

    Added comments to describe the program

--- random_name_generator_dictionary.py
+++ random_name_generator_dictionary.py
@@ -1,4 +1,6 @@
-#Importing random module
+# Program to randomly generate a name from a dictionary
+# Decided not to use this method as it was impractical and difficult to do compared to using the 2D list
+# Importing random module
 
 import random
 
@@ -17,13 +19,17 @@ numbers = [1, 2]
 # Loop for testing
 for i in range(3):
     gender = input("What type of name would you like? For a girl's name type <g>, for a boy's name type <b> or for a gender neutral name type <n>")
+    # Randomly generating a number which will correspond to a name
     number = random.choice(numbers)
+    # Generating a name if the user wants a girl's name
     if gender == "g":
         name = girls[number]
         print(name)
+    # Generating a name if the user wants a boy's name
     elif gender == "b":
         name = boys[number]
         print(name)
+    # Generating a name if the user wants a gender neutral name
     else:
         name = neutral[number]
         print(name)
