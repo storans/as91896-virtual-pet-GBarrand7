e813f9f4164bcdb383f16de3358459c78975dd78
GBarrand@storans.school.nz <GBarrand@storans.school.nz>
2019-10-31 09:49:08 +1300

    Added comments to describe the program

--- input_converter_function.py
+++ input_converter_function.py
@@ -1,3 +1,4 @@
+# Program to convert user input to the expected value when they enter a gender
 # Import random module
 
 import random
@@ -12,7 +13,9 @@ def name_generator(list_segment, list_name, message):
         # Randomly generating a name for the chosen gender
         name = random.choice(list_name[list_segment])
         print(name)
+        # Setting valid to false to run the loop
         valid = False
+        # Loop runs until user enters a valid input for yes or no
         while valid == False:
             # Asking if the user likes the name generated
             generate = input("{}".format(message)).strip().lower()
@@ -24,9 +27,10 @@ def name_generator(list_segment, list_name, message):
             elif generate in ["no", "n", "nah", ""]:
                 generate = ""
                 valid = True
+            # Branch which runs if the user does not enter any expected input which then loops back to ask the user for input again
             else:
                 print("Please enter either yes or no")
-
+    # Returning the approved name for use in the program
     return name
 
 # Function to convert user's gender input to the desired input if their input is within an approved list
@@ -36,25 +40,31 @@ def input_converter(question, error):
     valid = False
     # Function asks for input until the user inputs a valid response
     while valid == False:
+        # Asking user for input as to what gender name they would like
         gender = input(question).strip().lower()
         # Checking if the gender is within the approved list of inputs for female gender
         if gender in genders[0]:
             # Changing input to the standard input
             gender = "g"
+            # Returning the correct form of the desired gender
             return gender
         # Checking if the gender is within the approved list of inputs for male gender
         elif gender in genders[1]:
             # Changing input to the standard input
             gender = "b"
+            # Returning the correct form of the desired gender
             return gender
         # Checking if the gender is within the approved list of inputs for gender neutral
         elif gender in genders[2]:
             # Changing input to the standard input
             gender = "n"
+            # Returning the correct form of the desired gender
             return gender
+        # Branch which runs if the user does not enter any expected input
         else:
             print(error)
 
+# 2D list of expected inputs for each gender
 genders = [['g', 'girl', 'f', 'female'], ['b', 'boy', 'male', 'm'], ['n', 'neutral', 'gn', 'gender neutral']]
 
 # Using a 2D list to store the names
@@ -66,15 +76,19 @@ keep_going = ""
 
 # Loop for testing purposes
 while keep_going == "":
-    gender  = input_converter("Would you like a girl's name or a boy's name? Press <g> for girl, <b> for boy or <n> for a gender neutral name >>", "Please enter either <g>, <b> or <n>")
+    # Asking user for input as to what gender name they would like
+    gender = input_converter("Would you like a girl's name or a boy's name? Press <g> for girl, <b> for boy or <n> for a gender neutral name >>", "Please enter either <g>, <b> or <n>")
+    # Branch which runs if the user has selected a girl's name
     if gender == "g":
         # Randomly generating a name from the girl's name list until the user says they like the name using the name_generator function
         name = name_generator(0, names, "Do you like this name? Type 'yes' to choose this name or press <enter> to generate another name")
         print("Your cat's name is {}".format(name))
+    # Branch which runs if the user has selected a boy's name
     elif gender == "b":
         # Randomly generating a name from the boy's name list until the user says they like the name using the name_generator function
         name = name_generator(1, names, "Do you like this name? Type 'yes' to choose this name or press <enter> to generate another name")
         print("Your cat's name is {}".format(name))
+    # Branch which runs if the user has selected a gender neutral name
     elif gender == "n":
         # Randomly generating a name from the gender neutral name list until the user says they like the name using the name_generator function
         name = name_generator(2, names, "Do you like this name? Type 'yes' to choose this name or press <enter> to generate another name")
